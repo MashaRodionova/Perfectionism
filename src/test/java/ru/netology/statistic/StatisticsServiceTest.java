cf092ca14c5534365a906cd408b646b564728289
Masha Rodionova <rodionovamv@gmai.com>
2022-06-03 13:53:35 +0300

    Add tests, change counter

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,13 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxFirstTest() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxSecondTest() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInMillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInMillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxThirdTest() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInTrillions = {2, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInTrillions);
 
         assertEquals(expected, actual);
     }
